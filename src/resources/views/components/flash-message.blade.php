14c864ac635aa6e1eada2c2a93582feed8740bdc
Jens Ramakers <74543766+PS222407@users.noreply.github.com>
2022-12-01 10:48:43 +0100

    Update flash-message.blade.php

--- src/resources/views/components/flash-message.blade.php
+++ src/resources/views/components/flash-message.blade.php
@@ -2,27 +2,27 @@
     @switch(Session::get('type'))
         @case(Session::get('type') == 'error')
         <div class="bg-red-100 p-5 rounded-md shadow">
-            <p class="font-bold text-red-700"><i class="fa-solid fa-circle-xmark text-red-700 mr-2"></i>{{ Session::get('message') }}</p>
+            <p class="font-bold text-red-700"><i class="fa-solid fa-circle-xmark text-red-700 mr-2"></i>{!! Session::get('message') !!}</p>
         </div>
         @break
         @case(Session::get('type') == 'info')
         <div class="bg-sky-100 p-5 rounded-md shadow">
-            <p class="font-bold text-sky-700"><i class="fa-solid fa-circle-info text-sky-700 mr-2"></i>{{ Session::get('message') }}</p>
+            <p class="font-bold text-sky-700"><i class="fa-solid fa-circle-info text-sky-700 mr-2"></i>{!! Session::get('message') !!}</p>
         </div>
         @break
         @case(Session::get('type') == 'warning')
         <div class="bg-yellow-100 p-5 rounded-md shadow">
-            <p class="font-bold text-yellow-700"><i class="fa-solid fa-warning text-yellow-700 mr-2"></i>{{ Session::get('message') }}</p>
+            <p class="font-bold text-yellow-700"><i class="fa-solid fa-warning text-yellow-700 mr-2"></i>{!! Session::get('message') !!}</p>
         </div>
         @break
         @case(Session::get('type') == 'success')
         <div class="bg-emerald-100 p-5 rounded-md shadow">
-            <p class="font-bold text-emerald-700"><i class="fa-solid fa-circle-check text-emerald-700 mr-2"></i>{{ Session::get('message') }}</p>
+            <p class="font-bold text-emerald-700"><i class="fa-solid fa-circle-check text-emerald-700 mr-2"></i>{!! Session::get('message') !!}</p>
         </div>
         @break
         @case(Session::get('type') == 'stock')
         <div class="bg-red-100 p-5 rounded-md shadow">
-            <p class="font-bold text-red-700"><i class="fa-solid fa-circle-xmark text-red-700 mr-2"></i>{{ __('page.remove_items_before_checkout') }}</p>
+            <p class="font-bold text-red-700"><i class="fa-solid fa-circle-xmark text-red-700 mr-2"></i>{!! __('page.remove_items_before_checkout') !!}</p>
             <ul class="ml-8">
                 @foreach(Session::get('message') as $message)
                 <li class="font-bold text-red-700">- {!! $message !!}</li>
